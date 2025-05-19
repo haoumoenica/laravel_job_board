a6d1e904c48c27a82fae564ee5dd8dc3c90e1176
Moe Haounji <moe.haounji@gmail.com>
2025-05-19 15:25:48 +0200

    -adjusted to reusable code in the blade files - Added Alpine.js - Job controller adjustments, eager loading to get query for displaying jobs. -

--- resources/views/components/job-card.blade.php
+++ resources/views/components/job-card.blade.php
@@ -12,13 +12,21 @@
             <div>{{ $job->location }}</div>
         </div>
         <div class="flex space-x-1 text-xs">
-            <x-tag>{{ Str::ucfirst($job->experience) }}</x-tag>
-            <x-tag>{{ $job->category }}</x-tag>
+            <x-tag>
+
+                <a href="{{ route('jobs.index', ['experience' => $job->experience]) }}">
+                    {{ Str::ucfirst($job->experience) }}
+                </a>
+            </x-tag>
+            <x-tag>
+
+                <a href="{{ route('jobs.index', ['category' => $job->category]) }}">
+                    {{ Str::ucfirst($job->category) }}
+                </a>
+            </x-tag>
         </div>
 
     </div>
-
-    <p class="text-sm text-slate-500 mb-4">{!! nl2br($job->description) !!}</p>
 
     {{ $slot }}
 </x-card>
